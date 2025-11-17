405f01e1516c07fbe726b23495cdfaf67ef3261d
John <johnsneiider@gmail.com>
2025-11-17 14:11:43 -0500

    Corregir configuracion de app ai_trading en INSTALLED_APPS

--- ai_trading/__init__.py
+++ ai_trading/__init__.py
@@ -3,5 +3,3 @@ App de IA para trading con algoritmos genéticos y aprendizaje por refuerzo.
 Aplicación independiente que usa la misma base de datos del bot principal.
 """
 
-default_app_config = 'ai_trading.apps.AiTradingConfig'
-
--- bot_deriv/settings.py
+++ bot_deriv/settings.py
@@ -64,6 +64,7 @@ INSTALLED_APPS = [
     "dashboard",
     "notificaciones",
     "integracion_deriv",
+    "ai_trading.apps.AiTradingConfig",
 ]
 
 MIDDLEWARE = [
